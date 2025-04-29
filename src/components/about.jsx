db96d0fed214b146a28a78b0b8725f25a90e7e7e
susancantdance <susan.l.chen@gmail.com>
2025-04-29 14:33:48 -0700

    updated about copy

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -4,18 +4,15 @@ function About() {
   return (
     <>
       <p>
-        Hello! I'm Susan. I'm a computer science grad with an MBA (don't hold it
-        against me) looking for a software engineering opportunity. My extensive
-        career in tech includes data analysis, testing and optimization, and
-        product manangement.{" "}
+        Hello! I'm Susan. I'm looking for my next opportunity as a software
+        engineer. My experience in tech includes a CS degree and an extensive
+        career in web product manangement.{" "}
       </p>
       <p>
-        My enthusiasm for creating something from nothing has been a guiding
-        force in my life and career: From writing text-based games on a
-        Commodore 64 computer in the BASIC language -- to sewing plush toys to
-        raise money for charity organizations. As a product manager, I've worked
-        closely with engineers to design, build and launch a variety of
-        software.
+        From writing games in BASIC as a child to crafting plush toys for
+        charity, being creative has been a guiding force in my life as well as
+        my career. As a product manager, I've partnered closely with engineers
+        to design, build and launch software.
       </p>
       <p>
         {" "}
